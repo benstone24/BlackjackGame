58a19144808b8401df44af6f617f03c7b7412399
benstone24 <82447075+benstone24@users.noreply.github.com>
2021-10-07 14:09:37 +0100

    Update program.vb
    add banker twist & bust feature

--- Program.vb
+++ Program.vb
@@ -1,4 +1,4 @@
-Imports System.Threading 
+Imports System.Threading
 Imports System.Console
 Imports Microsoft.VisualBasic.CompilerServices
 
@@ -8,6 +8,7 @@ Module Program
     Dim PlayerTotal As Integer
     Dim BankCard1 As Integer
     Dim BankCard2 As Integer
+    Dim BankExtra As Integer
     Dim BankTotal As Integer
     Dim Random As New Random
     Dim Playing As Char = "y"
@@ -22,46 +23,46 @@ Module Program
             WriteLine("How much do you want to bet?")
             Bet = ReadLine()
             PlayerCard1 = Random.Next(1, 12)
-        Thread.Sleep(1000)
-        Clear()
-        For i = 1 To 3
-            WriteLine("Your 1st Card: .")
-            WriteLine("Your 2nd Card: ???")
-            Thread.Sleep(500)
+            Thread.Sleep(1000)
             Clear()
-            WriteLine("Your 1st Card: ..")
+            For i = 1 To 3
+                WriteLine("Your 1st Card: .")
+                WriteLine("Your 2nd Card: ???")
+                Thread.Sleep(500)
+                Clear()
+                WriteLine("Your 1st Card: ..")
+                WriteLine("Your 2nd Card: ???")
+                Thread.Sleep(500)
+                Clear()
+                WriteLine("Your 1st Card: ...")
+                WriteLine("Your 2nd Card: ???")
+                Thread.Sleep(500)
+                Clear()
+            Next
+            WriteLine("Your 1st Card: " & PlayerCard1)
             WriteLine("Your 2nd Card: ???")
-            Thread.Sleep(500)
-            Clear()
-            WriteLine("Your 1st Card: ...")
-            WriteLine("Your 2nd Card: ???")
-            Thread.Sleep(500)
-            Clear()
-        Next
-        WriteLine("Your 1st Card: " & PlayerCard1)
-        WriteLine("Your 2nd Card: ???")
 
-        PlayerCard2 = Random.Next(1, 12)
-        Clear()
-        Thread.Sleep(50)
-        For i = 1 To 3
-            WriteLine("Your 1st Card: " & PlayerCard1)
-            WriteLine("Your 2nd Card: .")
-            Thread.Sleep(500)
+            PlayerCard2 = Random.Next(1, 12)
             Clear()
+            Thread.Sleep(50)
+            For i = 1 To 3
+                WriteLine("Your 1st Card: " & PlayerCard1)
+                WriteLine("Your 2nd Card: .")
+                Thread.Sleep(500)
+                Clear()
+                WriteLine("Your 1st Card: " & PlayerCard1)
+                WriteLine("Your 2nd Card: ..")
+                Thread.Sleep(500)
+                Clear()
+                WriteLine("Your 1st Card: " & PlayerCard1)
+                WriteLine("Your 2nd Card: ...")
+                Thread.Sleep(500)
+                Clear()
+            Next
             WriteLine("Your 1st Card: " & PlayerCard1)
-            WriteLine("Your 2nd Card: ..")
-            Thread.Sleep(500)
-            Clear()
-            WriteLine("Your 1st Card: " & PlayerCard1)
-            WriteLine("Your 2nd Card: ...")
-            Thread.Sleep(500)
-            Clear()
-        Next
-        WriteLine("Your 1st Card: " & PlayerCard1)
-        WriteLine("Your 2nd Card: " & PlayerCard2)
-        Thread.Sleep(1000)
-        PlayerTotal = PlayerCard1 + PlayerCard2
+            WriteLine("Your 2nd Card: " & PlayerCard2)
+            Thread.Sleep(1000)
+            PlayerTotal = PlayerCard1 + PlayerCard2
             WriteLine("Your cards total to " & PlayerTotal & " .")
 
 
@@ -110,7 +111,20 @@ Module Program
             Thread.Sleep(1000)
             BankTotal = BankCard1 + BankCard2
             WriteLine("Bank's cards total to " & BankTotal & " .")
-            WriteLine("Your total is " & PlayerTotal)
+            If Random.Next(1, 3) = 5 And PlayerTotal > BankTotal Then 'Bank has 1/2 chance of twisting if bank is losing
+                WriteLine("Banker twisted!")
+                BankExtra = Random.Next(1, 12)
+                WriteLine("Banker got card " & BankExtra & " !")
+                BankTotal = BankTotal + BankExtra
+
+                If BankTotal > 21 Then
+                    WriteLine("Banker is bust! You win.")
+                    BankTotal = 0
+                End If
+            Else
+                WriteLine("Banker did not twist!")
+            End If
+            WriteLine("Your total Is " & PlayerTotal)
             If BankTotal >= PlayerTotal Then
                 WriteLine("You lost.")
                 Balance = Balance - Bet
@@ -126,13 +140,13 @@ Module Program
             End If
             countdown = 5
             Thread.Sleep(2500)
-                Clear()
-                WriteLine("You have £" & Balance)
-                WriteLine("Would you like to continue? (y/n)")
-                Thread.Sleep(5000)
-                Playing = ReadLine()
-                
-            
+            Clear()
+            WriteLine("You have £" & Balance)
+            WriteLine("Would you Like to continue? (y/n)")
+            Thread.Sleep(5000)
+            Playing = ReadLine()
+
+
 
 
 
